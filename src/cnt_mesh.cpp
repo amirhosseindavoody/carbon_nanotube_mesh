bca79c249a4025f59f5d269ca1a1cfc2c495e343
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2018-03-26 13:18:48 -0500

    debug

--- src/cnt_mesh.cpp
+++ src/cnt_mesh.cpp
@@ -338,7 +338,8 @@ void cnt_mesh::processCommandLineArgs(int argc, char* argv[])
 // function to set the output directory
 void cnt_mesh::set_output_dir(std::string output_path)
 {
-	output_directory = prepare_directory(output_path, keep_old_files=true);
+	bool keep_old_files=true;
+	output_directory = prepare_directory(output_path, keep_old_files);
 }
 
 
